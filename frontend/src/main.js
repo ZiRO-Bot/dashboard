819749ea71dde6e34f87932c3487cbc3b96823f8
ziro <palembani@gmail.com>
2021-10-03 18:35:48 +0700

    + More license stuff

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,21 @@
+/*
+   Z3R0's Dashboard
+   Copyright (C) 2021  null2264
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import axios from 'axios';
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
